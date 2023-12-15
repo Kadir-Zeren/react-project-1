e4777fe69f4c4cbc1fde31c0974fb3cc479a2625
kadir-zeren <k4dzeren@gmail.com>
2023-12-15 10:57:57 +0300

    Header com added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import Header from "./Header";
 function App() {
   return (
-    <div className="App">
-      <h1>Hello React</h1>
+    <div>
+      <Header />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
