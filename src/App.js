4b975e8f6ac0a00e3c87174703b05d20f86b6fd1
kadir-zeren <k4dzeren@gmail.com>
2023-12-15 11:26:32 +0300

    finished

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import Content from "./Content";
 import { Footer } from "./Footer";
 import Header from "./Header";
+
+//! CSS dosylari dogrudan import edilir. Export edilmelerine gerek yoktur.
+import "./App.css";
+
 function App() {
+  console.log("deneme");
   return (
     <div>
       <Header />
--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,5 +4,5 @@ export const Footer = () => {
     <footer>
       <p>Footer</p>
     </footer>
-  )
-}
+  );
+};
